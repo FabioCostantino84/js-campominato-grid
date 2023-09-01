5b1eac058c47a7fe106845765466a82e9283d45e
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-01 17:13:25 +0200

    add pseudo codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,9 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+//inserire bottone che genera una griglia di gioco
+
+//creare una griglia di 10 caselle per 10 righe e assegnare a ciascuna casella un numero da 1 a 100
+
+//al click su ugnuna casella si deve colorare di azzurro e stampare in console il numero della cella cliccata
