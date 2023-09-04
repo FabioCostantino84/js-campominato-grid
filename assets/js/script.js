1e894dabfe474b6776faed0f55c51da3b0baa253
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-04 13:15:56 +0200

    add generateBoxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,24 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+
+//creare una griglia di 10 caselle per 10 righe e assegnare a ciascuna casella un numero da 1 a 100
+const boxesElement = document.querySelector('.boxes')
+const limit = 100
+
+function generateboxes(domElement, limit) {
+
+    // creazione boxes
+    for (let i = 0; i < limit; i++) {
+
+        const cellElement = document.createElement('div')
+        cellElement.className = 'cell'
+        cellElement.append(i + 1)
+        domElement.append(cellElement)
+
+    }
+}
+
+// richiamo della funzione
+generateboxes(boxesElement, limit)
